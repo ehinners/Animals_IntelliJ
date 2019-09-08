ce8c2f586045a647efd2896be70977c40e8d329d
SHXBOT <ehinners@my.wctc.edu>
2019-09-08 14:35:22 -0500

    Changed if statements to a switch statement, used input class methods

--- src/us/mattgreen/Main.java
+++ src/us/mattgreen/Main.java
@@ -26,62 +26,39 @@ public class Main {
         String input;
         int age;
         int mice;
-        int iterations = 0;
         Boolean friendly;
+        char petType;
+        Input enterText = new Input();
         // Initialization of fields which will characterize objects/entries into the array
         boolean Loop = true;
         // Control variable to indicate whether the sentinel value has been entered
         do{
-
             System.out.println("Please Enter Name of Pet: (Enter NA to stop adding)");
             name = kb.nextLine();
 
             if(name.equals("NA"))
                 Loop = false;
-                // bypasses further inquiries if sentinel value has been entered
             else{
-                System.out.println("Is this a (S)tudent, (C)at, or (D)og?");
-                input = kb.nextLine();
-                // indicates pet type, admittedly, in hindsight this would be better as a switch statement
-                if(input.charAt(0)=='s'|| input.charAt(0)=='S')
+                petType = enterText.petSwitch("Is this a (S)tudent, (C)at, or (D)og?", 'S', 99);
+                switch (petType)
                 {
-                    System.out.println("What is the Student's Age?");
-                    try {
-                        age = Integer.parseInt(kb.nextLine());
-                    }
-                    catch(Exception e)
-                    {
-                        System.out.println("You Must Be 18");
-                        age = 18;
-                    }
+                    case 's':
+                    case 'S':
+                        age = enterText.verifyInt("What is the Student's Age?", 18, 99);
+                        zoo.add(new Student(age, name));
+                        break;
 
-                    zoo.add(new Student(age,name));
-                }
-                if(input.charAt(0)=='c'|| input.charAt(0)=='C')
-                {
-                    System.out.println("How many mice has the cat killed?");
-                    try {
-                        mice = Integer.parseInt(kb.nextLine());
-                    }
-                    catch(Exception e)
-                    {
-                        System.out.println("Mice killed defaulted to 0");
-                        mice = 0;
-                    }
+                    case 'c':
+                    case 'C':
+                        mice = enterText.verifyInt("How many mice has the cat killed?", 0, 99);
+                        zoo.add(new Cat(mice, name));
+                        break;
 
-                    zoo.add(new Cat(mice,name));
-                }
-                if(input.charAt(0)=='d'|| input.charAt(0)=='D')
-                {
-                    System.out.println("Is the Dog Friendly?");
-                    input = kb.nextLine();
-
-                    if(input.charAt(0)=='y'||input.charAt(0)=='Y')
-                        friendly = true;
-                    else
-                        friendly = false;
-
-                    zoo.add(new Dog(friendly,name));
+                    case 'd':
+                    case 'D':
+                        friendly = enterText.verifyYesNo("Is the Dog Friendly?", true, 99);
+                        zoo.add(new Dog(friendly, name));
+                        break;
                 }
             }
         }while(Loop);
